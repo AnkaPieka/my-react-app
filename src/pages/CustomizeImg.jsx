90b49bd7adf7e06bbe1a0344e15308114e1cee1f
Anna Lefour <anna.lefour@laposte.net>
2021-04-05 16:18:57 +0200

    task 1 ok

--- src/pages/CustomizeImg.jsx
+++ src/pages/CustomizeImg.jsx
@@ -9,3 +9,4 @@ function CustomizeImg() {
 }
 
 export default CustomizeImg
+
--- src/pages/Temperature.jsx
+++ src/pages/Temperature.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { PureComponent } from "react";
 
-function Temperature() {
+
+export class Temperature extends PureComponent {
+  state = {
+    temperature: 0,
+  };
+
+  handleChange = (event) => {
+    const writtenTemp = event.target.value;
+    this.setState({ temperature: writtenTemp });
+  };
+
+  phrase = () => {
+    if (this.state.temperature < 10) {
+      return <p className="cold-phrase">"It's cold ❄️"</p>;
+    } else if (this.state.temperature >= 10 && this.state.temperature <= 30) {
+      return <p className="nice-phrase">It nice 🌼"</p>;
+    } else if (this.state.temperature > 30) {
+      return <p className="warm-phrase">"It's warm ☀️"</p>;
+    }
+  };
+
+  render() {
     return (
-        <div>
-            <h1>Temperature</h1>
+      <div>
+        {console.log(this.state.temperature)}
+        <h1>Temperature</h1>
+        <div className="container">
+          <input
+            onChange={this.handleChange}
+            type="number"
+            placeholder="Temp in celsius"
+            className="temperature-input"
+          ></input>
+          <div>{this.phrase()}</div>
         </div>
-    )
+      </div>
+    );
+  }
 }
 
-export default Temperature
+export default Temperature;
